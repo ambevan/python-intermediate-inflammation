6ec045c71e834922c5895f50d2e0364c7a48ab4e
Antonia Bevan <kzht939@astrazeneca.net>
2022-02-15 14:17:39 +0000

    Corrected error in daily_max()

--- tests/test_models.py
+++ tests/test_models.py
@@ -38,10 +38,10 @@ def test_daily_mean_integers():
         ([[-1, 2], [3, -4], [-5, 6]], [3, 6])
     ]
 )
-def test_daily_min(test, expected):
+def test_daily_max(test, expected):
     """Test that max function works for an array of zeros, and positive and negative integers, and floats."""
-    from inflammation.models import daily_min
-    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+    from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 
 @pytest.mark.parametrize(
